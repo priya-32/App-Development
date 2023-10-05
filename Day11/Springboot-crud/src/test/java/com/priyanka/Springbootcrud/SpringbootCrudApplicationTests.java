962e609b2633169698237f95adc50aaf718cff1d
priya-32 <120549903+priya-32@users.noreply.github.com>
2023-10-05 17:47:08 +0530

    Add files via upload

---
+++ Day11/Springboot-crud/src/test/java/com/priyanka/Springbootcrud/SpringbootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyanka.Springbootcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
